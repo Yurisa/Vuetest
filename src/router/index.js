f7ce4a5b52ff3523cfdcf3b6558c06996603cd65
Yurisa <779509411@qq.com>
2018-01-31 14:19:45 +0800

    加入头部底部面包屑组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import GoodsList from './../views/goodList'
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'GoodList',
+      component:GoodsList 
+        }
   ]
 })
