472999c051cefa374cd53ee978319993c171ccb6
Yurisa <779509411@qq.com>
2018-02-28 21:32:44 +0800

    解决跨域问题

--- config/index.js
+++ config/index.js
@@ -11,9 +11,9 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/goods':{
-        target:"http://localhost:3000/"
-      }
+      // '/goods':{
+      //   target:"http://localhost:3000/"
+      // }
     },
 
     // Various Dev Server settings
--- server/app.js
+++ server/app.js
@@ -24,23 +24,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req,res,next) {
-  if(req.cookies.userId){
-    next();
-  }else{
-      console.log("url:"+req.originalUrl);
-      if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
-          next();
-      }else{
-          res.json({
-            status:'10001',
-            msg:'当前未登录',
-            result:''
-          });
-      }
-  }
+// app.use(function (req,res,next) {
+//   if(req.cookies.userId){
+//     next();
+//   }else{
+//       console.log("url:"+req.originalUrl);
+//       if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
+//           next();
+//       }else{
+//           res.json({
+//             status:'10001',
+//             msg:'当前未登录',
+//             result:''
+//           });
+//       }
+//   }
+// });
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
-
 app.use('/', index);
 app.use('/users', users);
 app.use('/goods', goods);
